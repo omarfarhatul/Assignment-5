38ac4b187939687d313edf137069e265229ce3eb
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-07 15:15:04 +0600

    update

--- js/utilities.js
+++ js/utilities.js
@@ -1,13 +1,11 @@
 function getInputFieldValueById(id){
     const inputValue=document.getElementById(id).value;
-
     return inputValue;
 }
 
 function getTextFieldValueById(id){
     const textValue=document.getElementById(id).innerText;
     const textNumber=parseFloat(textValue);
-
     return textNumber;
 }
 
